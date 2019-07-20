61e8149022257f875d2c4db60d2687216d1ce175
McHaris <harismanaf96@gmail.com>
2019-07-20 19:17:09 +0700

    create Project

---
+++ src/main/java/com/eksad/ServerSidePaging/ServerSidePagingApplication.java
@@ -0,0 +1,13 @@
+package com.eksad.ServerSidePaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSidePagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSidePagingApplication.class, args);
+	}
+
+}
